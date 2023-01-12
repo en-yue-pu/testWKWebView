20e11e782860c6e4929f956a519897c2b6887c86
yue_pu <yue_pu@en-japan.com>
2023-01-12 21:49:11 +0900

    显示谷歌页面

--- testWKWebview/ViewController.swift
+++ testWKWebview/ViewController.swift
@@ -6,12 +6,18 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let webView = WKWebView(frame:UIScreen.main.bounds)
+        let url = URL(string: "http://www.google.co.jp")!
+        let request = URLRequest(url: url)
+        webView.load(request)
+        view.addSubview(webView)
     }
 
 
