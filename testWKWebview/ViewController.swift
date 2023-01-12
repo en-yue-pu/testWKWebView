eaf976455ac02bec47a6f58cdf611d58a345f920
yue_pu <yue_pu@en-japan.com>
2023-01-12 22:03:23 +0900

    native发送html代码给webView执行,取得数据打印(数据是全网也html)

--- testWKWebview/ViewController.swift
+++ testWKWebview/ViewController.swift
@@ -10,6 +10,8 @@ import WebKit
 
 class ViewController: UIViewController {
     
+    var webView: WKWebView!
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -17,9 +19,18 @@ class ViewController: UIViewController {
         let url = URL(string: "http://www.google.co.jp")!
         let request = URLRequest(url: url)
         webView.load(request)
+        self.webView = webView
         view.addSubview(webView)
+        
+        //native发送html代码给webView执行,取得数据打印(数据是全网也html)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+            //这里有个小坑,要用self.webView不能用webView因为viewDidLoad函数执行完了实例消失,闭包里面是弱引用,所以webView会消失,事先传到到外界的webView因为class级别的引用还在所以没事
+            self.webView.evaluateJavaScript("document.body.innerHTML") { result , error in
+                guard let html = result as? String, error == nil else { return }
+                print(html)
+            }
+        }
     }
-
 
 }
 
