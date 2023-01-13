c4d7a8d28c5442e00c1cb1faa0b49dcc87612b41
yue_pu <yue_pu@en-japan.com>
2023-01-13 14:31:43 +0900

    用本地服务访问html

--- testWKWebview/ViewController.swift
+++ testWKWebview/ViewController.swift
@@ -10,15 +10,23 @@ import WebKit
 
 class ViewController: UIViewController {
     
-    let webView = WKWebView(frame:UIScreen.main.bounds)
+    var webView: WKWebView!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        let url = URL(string: "http://www.google.co.jp")!
+        
+        let webConfig: WKWebViewConfiguration = WKWebViewConfiguration()
+        let userController: WKUserContentController = WKUserContentController()
+        userController.add(self, name: "hoge")
+        webConfig.userContentController = userController
+        webView = WKWebView(frame:  UIScreen.main.bounds, configuration: webConfig)
+
+        let url = URL(string: "http://localhost:9999/web.html")!
         let request = URLRequest(url: url)
         webView.load(request)
-        webView.customUserAgent = "Chrome/Firefox"//"iPad" iPhone //限定网页显示模式
+        webView.customUserAgent = "iPhone" //"Chrome/Firefox"//"iPad" iPhone //限定网页显示模式
+        
         view.addSubview(webView)
         
         //native发送html代码给webView执行,取得数据打印(数据是全网也html)
@@ -30,5 +38,14 @@ class ViewController: UIViewController {
         }
     }
 
+}
+
+extension ViewController: WKScriptMessageHandler {
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+            if message.name == "hoge" {
+                let number = message.body as! String
+                // do something
+            }
+        }
 }
 
