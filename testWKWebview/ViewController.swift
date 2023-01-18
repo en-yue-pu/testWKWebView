b92d8d7a9c5d4fcafe4e5309f4d41652c3fb27f6
yue_pu <yue_pu@en-japan.com>
2023-01-18 14:42:14 +0900

    改进 追加 显示 toast

--- testWKWebview/ViewController.swift
+++ testWKWebview/ViewController.swift
@@ -34,21 +34,27 @@ class ViewController: UIViewController {
         view.addSubview(webView)
                 
         //native发送html代码给webView执行,取得数据打印(数据是全网也html)
-        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
-            self.webView.evaluateJavaScript("document.body.innerHTML") { result , error in
-                guard let html = result as? String, error == nil else { return }
-                print(html)
-            }
-        }
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+//            self.webView.evaluateJavaScript("document.body.innerHTML") { result , error in
+//                guard let html = result as? String, error == nil else { return }
+//                print(html)
+//            }
+//        }
     }
 }
 
 extension ViewController: WKNavigationDelegate {
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        // native调用webview里面的sayHello()函数,参数是一个string
+        /* 载入结束就
+        native调用webview 里面的sayHello()函数,参数是一个string(native发送给webView)
+         */
         webView.evaluateJavaScript("sayHello('native发送给webView')") { (result, err) in
-            print(result, err)
+            print("native > webview の　callback:", result, err)
         }
+    }
+    
+    func webView(_ webView: WKWebView, didReceive challenge: URLAuthenticationChallenge) {
+        print("######")
     }
 }
 
@@ -58,8 +64,29 @@ extension ViewController: WKScriptMessageHandler {
             if message.name == "hoge" {
                 let str = message.body as! String
                 // do something
-                print("$$$$$$$$$$$", str)
+                print("webView > native:", str)
+                self.showToast(message: str)
             }
         }
 }
 
+extension ViewController {
+
+func showToast(message : String, font: UIFont = .systemFont(ofSize: 12.0)) {
+
+    let toastLabel = UILabel(frame: CGRect(x: self.view.frame.size.width/2 - 75, y: self.view.frame.size.height-100, width: 150, height: 35))
+    toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+    toastLabel.textColor = UIColor.white
+    toastLabel.font = font
+    toastLabel.textAlignment = .center;
+    toastLabel.text = message
+    toastLabel.alpha = 1.0
+    toastLabel.layer.cornerRadius = 10;
+    toastLabel.clipsToBounds  =  true
+    self.view.addSubview(toastLabel)
+    UIView.animate(withDuration: 4.0, delay: 0.1, options: .curveEaseOut, animations: {
+         toastLabel.alpha = 0.0
+    }, completion: {(isCompleted) in
+        toastLabel.removeFromSuperview()
+    })
+} }
